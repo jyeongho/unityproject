2594c3b05f2b8d8fad54f8ed47b8fce243ad6543
24seconds <24crazyoung@gmail.com>
2018-07-14 15:22:14 +0900

    changed
    script

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    GameObject Walls;
+
     public float speed;
 
+    private int i = 0;
+    private bool isMove = false;
     private Rigidbody rb;
+
+    private void Awake()
+    {
+        Walls = GameObject.FindGameObjectWithTag("Walls");
+    }
 
     void Start()
     {
@@ -16,11 +25,67 @@ public class Player : MonoBehaviour {
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        if(Input.GetKeyDown(KeyCode.LeftArrow) == true && !isMove)
+        {
+            i = 1;
+            isMove = true;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) == true && !isMove)
+        {
+            i = 2;
+            isMove = true;
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow) == true && !isMove)
+        {
+            i = 3;
+            isMove = true;       
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow) == true && !isMove)
+        {
+            i = 4;
+            isMove = true;          
+        }
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        switch (i)
+        {
+            case 1:
+                if (isMove)
+                {
+                    transform.Translate(Vector3.left * speed * Time.deltaTime);
+                }
+                break;
+            case 2:
+                if (isMove)
+                {
+                    transform.Translate(-Vector3.left * speed * Time.deltaTime);
+                }
+                break;
+            case 3:
+                if (isMove)
+                {
+                    transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                }
+                break;
+            case 4:
+                if (isMove)
+                {
+                    transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+                }
+                break;
+        }
 
-        rb.AddForce(movement * speed);
+        if (!isMove)
+        {
+            
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.transform.tag == "Walls")
+        {
+            isMove = false;
+            Debug.Log(isMove.ToString());
+        }
     }
 }
