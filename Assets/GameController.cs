781c1b62034e9b65842a9a087aeec0c5d7f9ce12
jyeongho <jyeongho@users.noreply.github.com>
2018-07-14 23:57:16 +0900

    First merge, not works so well

---
+++ Assets/GameController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System;
+
+public class GameController : MonoBehaviour {
+
+    private InputField input;
+    public Text text;
+    public Text limitTimer;
+    private float startTime;
+    public Button button;
+    
+    private void Awake()
+    {
+        input = GameObject.Find("InputField").GetComponent<InputField>();
+        button.enabled = false;
+    }
+    public void GetInput(string str)
+    {
+        if (text.text == "Shortest path")
+        {
+            text.text = str;
+            startTime = Time.time;
+        } else
+        {
+            if (Int32.Parse(text.text) > Int32.Parse(str))
+            {
+                text.text = str;
+                startTime = Time.time;
+            }
+        }
+        input.text = "";
+    }
+
+    private void Update()
+    {
+        if (text.text != "Shortest path")
+        {
+            float t = Time.time - startTime;
+
+            string minutes = ((int)t / 60).ToString();
+            string seconds = (t % 60).ToString("f2");
+            limitTimer.text = minutes + ":" + seconds;
+            if (minutes == "0")
+            {
+                limitTimer.text = "Time over!";
+                button.enabled = true;
+            }
+        }
+    }
+}
--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -22,6 +22,15 @@ public class Player : MonoBehaviour {
     float speed3;
     float speed4;
     private GameObject currentcamera;
+    private bool button;
+    private int i = 0;
+    private bool isMove;
+    private int n = 0;
+
+    private void Awake()
+    {
+        isMove = false;
+    }
 
     void Start()
     {
@@ -33,46 +42,106 @@ public class Player : MonoBehaviour {
         rb3.freezeRotation = true;
         rb4 = Player4.GetComponent<Rigidbody>();
         rb4.freezeRotation = true;
-        speed1 = 10;
+        speed1 = 0;
         speed2 = 0;
         speed3 = 0;
         speed4 = 0;
+        currentcamera = defaultcamera;
+        button = false;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown("r"))
+        if (button)
         {
-            switchToPlayer(cameraplayer1);
-            speed1 = 10;
-            speed2 = 0;
-            speed3 = 0;
-            speed4 = 0;
+            if (Input.GetKeyDown("r"))
+            {
+                switchToPlayer(cameraplayer1);
+                speed1 = 10;
+                speed2 = 0;
+                speed3 = 0;
+                speed4 = 0;
+            }
+            if (Input.GetKeyDown("b"))
+            {
+                switchToPlayer(cameraplayer2);
+                speed1 = 0;
+                speed2 = 10;
+                speed3 = 0;
+                speed4 = 0;
+            }
+            if (Input.GetKeyDown("y"))
+            {
+                switchToPlayer(cameraplayer3);
+                speed1 = 0;
+                speed2 = 0;
+                speed3 = 10;
+                speed4 = 0;
+            }
+            if (Input.GetKeyDown("g"))
+            {
+                switchToPlayer(cameraplayer4);
+                speed1 = 0;
+                speed2 = 0;
+                speed3 = 0;
+                speed4 = 10;
+            }
         }
 
-        if (Input.GetKeyDown("b"))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) == true && !isMove)
         {
-            switchToPlayer(cameraplayer2);
-            speed1 = 0;
-            speed2 = 10;
-            speed3 = 0;
-            speed4 = 0;
+            i = 1;
+            isMove = true;
+            button = false;
         }
-        if (Input.GetKeyDown("y"))
+        if (Input.GetKeyDown(KeyCode.RightArrow) == true && !isMove)
         {
-            switchToPlayer(cameraplayer3);
-            speed1 = 0;
-            speed2 = 0;
-            speed3 = 10;
-            speed4 = 0;
+            i = 2;
+            isMove = true;
+            button = false;
         }
-        if (Input.GetKeyDown("g"))
+        if (Input.GetKeyDown(KeyCode.UpArrow) == true && !isMove)
         {
-            switchToPlayer(cameraplayer4);
-            speed1 = 0;
-            speed2 = 0;
-            speed3 = 0;
-            speed4 = 10;
+            i = 3;
+            isMove = true;
+            button = false;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) == true && !isMove)
+        {
+            i = 4;
+            isMove = true;
+            button = false;
+        }
+
+        if (isMove)
+        {
+            switch (i)
+            {
+                case 1:
+                    transform.Translate(Vector3.left * speed1 * Time.deltaTime);
+                    transform.Translate(Vector3.left * speed2 * Time.deltaTime);
+                    transform.Translate(Vector3.left * speed3 * Time.deltaTime);
+                    transform.Translate(Vector3.left * speed4 * Time.deltaTime);
+                    break;
+                case 2:
+                    transform.Translate(-Vector3.left * speed1 * Time.deltaTime);
+                    transform.Translate(-Vector3.left * speed2 * Time.deltaTime);
+                    transform.Translate(-Vector3.left * speed3 * Time.deltaTime);
+                    transform.Translate(-Vector3.left * speed4 * Time.deltaTime);
+                    break;
+                case 3:
+                    transform.Translate(Vector3.forward * speed1 * Time.deltaTime);
+                    transform.Translate(Vector3.forward * speed2 * Time.deltaTime);
+                    transform.Translate(Vector3.forward * speed3 * Time.deltaTime);
+                    transform.Translate(Vector3.forward * speed4 * Time.deltaTime);
+                    break;
+                case 4:
+                    transform.Translate(-Vector3.forward * speed1 * Time.deltaTime);
+                    transform.Translate(-Vector3.forward * speed2 * Time.deltaTime);
+                    transform.Translate(-Vector3.forward * speed3 * Time.deltaTime);
+                    transform.Translate(-Vector3.forward * speed4 * Time.deltaTime);
+                    break;
+            }
         }
 
     }
@@ -84,6 +153,7 @@ public class Player : MonoBehaviour {
 
     void FixedUpdate()
     {
+        /*
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
@@ -93,8 +163,111 @@ public class Player : MonoBehaviour {
         rb2.AddForce(movement * speed2);
         rb3.AddForce(movement * speed3);
         rb4.AddForce(movement * speed4);
+        */
         defaultcamera.transform.position = currentcamera.transform.position;
         defaultcamera.transform.rotation = currentcamera.transform.rotation;
 
+    }
+
+
+    public void buttonOn()
+    {
+        button = true;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(i.ToString());
+        if (i > 0) {
+            if ((i < 3) && (other.transform.tag == "VerticalWalls"))
+        {
+                //move to center
+                if (i == 1)
+                {
+                    transform.position = Vector3.MoveTowards
+                        (transform.position,
+                        transform.position + new Vector3(0.135f, 0, 0),
+                        speed1 * Time.deltaTime);
+                    transform.position = Vector3.MoveTowards
+    (transform.position,
+    transform.position + new Vector3(0.135f, 0, 0),
+    speed2 * Time.deltaTime);
+                    transform.position = Vector3.MoveTowards
+    (transform.position,
+    transform.position + new Vector3(0.135f, 0, 0),
+    speed3 * Time.deltaTime);
+                    transform.position = Vector3.MoveTowards
+    (transform.position,
+    transform.position + new Vector3(0.135f, 0, 0),
+    speed4 * Time.deltaTime);
+                }
+                else
+                {
+                    transform.position = Vector3.MoveTowards
+                        (transform.position,
+                        transform.position + new Vector3(-0.135f, 0, 0),
+                        speed1 * Time.deltaTime);
+                    transform.position = Vector3.MoveTowards
+    (transform.position,
+    transform.position + new Vector3(-0.135f, 0, 0),
+    speed2 * Time.deltaTime);
+                    transform.position = Vector3.MoveTowards
+    (transform.position,
+    transform.position + new Vector3(-0.135f, 0, 0),
+    speed3 * Time.deltaTime);
+                    transform.position = Vector3.MoveTowards
+    (transform.position,
+    transform.position + new Vector3(-0.135f, 0, 0),
+    speed4 * Time.deltaTime);
+                }
+            }
+            if ((i >= 3) && (other.transform.tag == "HorizontalWalls"))
+            {
+                //move to center
+                if (i == 3)
+                {
+                    transform.position = Vector3.MoveTowards
+                        (transform.position,
+                        transform.position + new Vector3(0, 0, -0.045f),
+                        speed1 * Time.deltaTime);
+                    transform.position = Vector3.MoveTowards
+    (transform.position,
+    transform.position + new Vector3(0, 0, -0.045f),
+    speed2 * Time.deltaTime);
+                    transform.position = Vector3.MoveTowards
+    (transform.position,
+    transform.position + new Vector3(0, 0, -0.045f),
+    speed3 * Time.deltaTime);
+                    transform.position = Vector3.MoveTowards
+    (transform.position,
+    transform.position + new Vector3(0, 0, -0.045f),
+    speed4 * Time.deltaTime);
+                }
+                else
+                {
+                    transform.position = Vector3.MoveTowards
+                        (transform.position,
+                        transform.position + new Vector3(0, 0, 0.045f),
+                        speed1 * Time.deltaTime);
+                    transform.position = Vector3.MoveTowards
+    (transform.position,
+    transform.position + new Vector3(0, 0, 0.045f),
+    speed2 * Time.deltaTime);
+                    transform.position = Vector3.MoveTowards
+    (transform.position,
+    transform.position + new Vector3(0, 0, 0.045f),
+    speed3 * Time.deltaTime);
+                    transform.position = Vector3.MoveTowards
+    (transform.position,
+    transform.position + new Vector3(0, 0, 0.045f),
+    speed4 * Time.deltaTime);
+                }
+            }
+        }
+
+        button = true;
+        isMove = false;
+        n += 1;
+        i = 0;
     }
 }
