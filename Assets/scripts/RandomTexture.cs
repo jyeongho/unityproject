068043e733ed1cdc3ec519ee194d3753bc73bedd
24seconds <24crazyoung@gmail.com>
2018-07-15 01:27:50 +0900

    Add tokens

---
+++ Assets/scripts/RandomTexture.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTexture : MonoBehaviour {
+
+    GameObject[] tokens;
+    GameObject currentObject;
+    int index;
+
+
+
+	// Use this for initialization
+	void Start () {
+        tokens = GameObject.FindGameObjectsWithTag("TokenTag");
+        index = Random.Range(0, tokens.Length);
+
+        foreach(GameObject item in tokens)
+        {
+            Debug.Log(item.name);
+            Debug.Log(item.name.Substring(0,1));
+        }
+
+        Debug.Log("-----------------------");
+
+        currentObject = Instantiate(tokens[index]);
+
+        Debug.Log(currentObject.name);
+       
+        currentObject.transform.position = new Vector3(0.5f, 0.5f, 0.0f);
+
+	}
+
+    private void Update()
+    {
+        
+
+    }
+
+
+
+
+
+
+
+
+
+}
